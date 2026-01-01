8e4c0e1a5ef8909f667dce79e873e42067051c94
HarshaanNiles010 <harshaanniles010@gmail.com>
2026-01-01 13:40:36 +0000

    Day-3 part 2 not functional

--- AOC-2016/Day-3/part1.py
+++ AOC-2016/Day-3/part1.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    with open('input.txt') as f:
+        triangles = [i.strip().split() for i in f.readlines()]
+    def validate_triangle(a: int, b: int, c: int) -> bool:
+        return (a + b > c) and (a + c > b) and (b + c > a)
+    count = 0
+    for triangle in triangles:
+        if validate_triangle(int(triangle[0]), int(triangle[1]), int(triangle[2])):count += 1
+        else: continue
+    print(count)
 
 if __name__ == '__main__':
     main()
