3eff7784b63bc5b1574620f2c74426d24ad4bca5
HarshaanNiles010 <harshaanniles010@gmail.com>
2026-01-03 16:40:32 +0530

    Solving Day-7

--- AOC-2016/Day-7/part1.py
+++ AOC-2016/Day-7/part1.py
@@ -1,5 +1,14 @@
+import re
 def main():
-    pass
-
+    with open('input.txt') as f:
+        lines = [i.strip() for i in f.readlines()]
+    data = {}
+    # Step 1 check if there are any palindromes within the square brackets, if they are, then remove those IP Addresses
+    for i,line in enumerate(lines):
+        t = re.findall(r"\[\w+\]", line)
+        for j in t:
+            x = j.replace('[', ' ').replace(']', ' ').strip()
+            if x != x[::-1]:data[i] = line
+    print(data)
 if __name__ == '__main__':
     main()
